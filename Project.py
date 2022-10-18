82599fe4c4c84d9047db9f443eb2ac34ae6ff7a8
walldemarsokol <walldemarsokol@yandex.ru>
2022-10-18 17:21:44 +0300

    add_project

---
+++ Project.py
@@ -0,0 +1,22 @@
+
+
+list=[] 
+list_1=(input('Введите слово для игры: '))
+list_1,list_2=list_1.find('п'),list_1.find('а')
+# for i in range(len(list_1)):
+#     list.append(list_1[i])
+
+# for j in range(len(list)):
+    
+# path = 'Names.txt'
+# data=open(path,'r', encoding='utf-8')
+# for line in data:
+#     if list.find(path)==True:
+#         print(list.find(path))
+#     # print(line)
+# data.close()
+# print(list.find(open('Names.txt','r')))
+print(list_1,list_2)
+# print(list_2)
+
+
