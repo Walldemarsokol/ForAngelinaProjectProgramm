4c70dfd1bdd5bd0fd811c8e1bb26259e0da622fe
walldemarsokol <walldemarsokol@yandex.ru>
2022-11-03 20:43:24 +0300

    redact project

--- Project.py
+++ Project.py
@@ -1,8 +1,6 @@
 
 
-from asyncore import write
 from encodings import utf_8
-from fileinput import close
 
 
 list=[] 
@@ -10,25 +8,10 @@ list_1=(input('Введите слово для игры: '))
 for i in range(len(list_1)):
     list.append(list_1[i])
 list_2=['а','д','е','л','а','и','д','а']
-# with open ('Names.txt', 'r', encoding=utf_8):
-for j in range(len(list_1)):
-    if list[j]==list_2[5]:
-        print('Аделаида')
-# for j in range(len(list)):
-#     if list[j]==list_2[5]:
-#         print('Аделаида')
-# for j in range(len(list)):
-    
-# path = 'Names.txt'
-# data=open(path,'r', encoding='utf-8')
-# for line in data:
-#     if list.find(path)==True:
-#         print(list.find(path))
-#     # print(line)
-# data.close()
-# print(list.find(open('Names.txt','r')))
-# print(len(list))
+with open ('Names.txt', 'r', encoding=utf_8):
+    for j in range(len(list_1)):
+        if list[j]==list_2[5]:
+            print('Аделаида')
 
-print(list)
 
 
