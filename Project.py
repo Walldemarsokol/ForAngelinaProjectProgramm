5b964f525006043404d081d5a4862a376baf3205
walldemarsokol <walldemarsokol@yandex.ru>
2022-11-03 20:50:12 +0300

    redact_project

--- Project.py
+++ Project.py
@@ -7,11 +7,13 @@ list=[]
 list_1=(input('Введите слово для игры: '))
 for i in range(len(list_1)):
     list.append(list_1[i])
-list_2=['а','д','е','л','а','и','д','а']
-with open ('Names.txt', 'r', encoding=utf_8):
-    for j in range(len(list_1)):
-        if list[j]==list_2[5]:
-            print('Аделаида')
+
+print(list)
+# list_2=['а','д','е','л','а','и','д','а']
+# with open ('Names.txt', 'r', encoding='utf-8'):
+#     for j in range(len(list_1)):
+#         if list[j]==list_2[5]:
+#             print('Аделаида')
 
 
 
