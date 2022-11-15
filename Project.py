22c7469491e560bdbacef29a23640b7de274c9ad
walldemarsokol <walldemarsokol@yandex.ru>
2022-11-15 20:24:50 +0300

    add scan_method and correct Project

--- Project.py
+++ Project.py
@@ -1,19 +1,14 @@
-
-
-from encodings import utf_8
-
-
 list=[] 
 list_1=(input('Введите слово для игры: '))
 for i in range(len(list_1)):
     list.append(list_1[i])
 
 print(list)
-# list_2=['а','д','е','л','а','и','д','а']
-# with open ('Names.txt', 'r', encoding='utf-8'):
-#     for j in range(len(list_1)):
-#         if list[j]==list_2[5]:
-#             print('Аделаида')
+list_2=['а','д','е','л','а','и','д','а']
+with open ('Names.txt', 'r', encoding='utf-8'):
+    for j in range(len(list_1)):
+        if list[j]==list_2[5]:
+            print('Аделаида')
 
 
 
