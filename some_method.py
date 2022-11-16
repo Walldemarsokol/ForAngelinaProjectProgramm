7352224c201abb277241fa7bb91aed93281d2063
walldemarsokol <walldemarsokol@yandex.ru>
2022-11-16 20:21:52 +0300

    correct_files

--- some_method.py
+++ some_method.py
@@ -1,32 +1,7 @@
-import random
-import math #подключение нужных модулей
-from the_3_step import the_3_step
+from the_2nd_method import the_2nd_method
 
-L=input("enter text: ") # вводим текст, вводим в кавычках
+L=input("Please, enter text: ") # вводим текст, вводим в кавычках
 D=list(L) # преобразуем в список
-i=0 # счетчик
-k=1
-Y=[] # пустой список
-F=[]
-G=[]
 l=len(D) # длина введенного текста(количество элементов списка)
+the_2nd_method(D,l)
 
-for i in range(l): # делаем пока не исчерпаем все варианты
-    G.append(D[i])
-    print(f'check I ={i+1}')
-    print(G)
-    a=math.factorial(k)
-    print(f'factorial={a}')
-    k+=1
-    z=0
-    the_3_step(a,G,z)
-    # while z<a:
-    #     print(f'check={z+1}')
-    #     random.shuffle(G) # мешаем список
-    #     F=(''.join(G))
-    #     if F in Y: # если "пар" есть в списке Y
-    #         continue #начнем мешать элементы списка сначала
-    #     else: #если не нашел
-    #         Y[len(Y):]=[F]  #добавляем в конец списка  Y
-    #         print (F) #печатаем F, можно распечатать и пронумеровать элементы списка Y 
-    #         z+=1
--- the_2nd_method.py
+++ the_2nd_method.py
@@ -1,9 +1,15 @@
 import math
-import the_3_step
+from the_3_step import the_3_step
 
-def the_2nd_method():
+def the_2nd_method(input_list,length):
+    g=[]
     k=1
-    j=2
-    while k<j:
-        # l=math.factorial(l) # количество вариантов написания
+    for i in range(length): 
+        g.append(input_list[i])
+        print(f'check I ={i+1}')
+        print(g)
         a=math.factorial(k)
+        print(f'factorial={a}')
+        k+=1
+        z=0
+        the_3_step(a,g,z)
