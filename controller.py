69a56aaf8f3ef419d7e87854c8edb2e553d97812
walldemarsokol <bergamot992@gmail.com>
2023-07-25 23:13:24 +0300

    решил заменить текстовые файлы на словари

--- controller.py
+++ controller.py
@@ -1,5 +1,12 @@
-from find_method import find_method
-from gen_method import gen_method
+# from find_method import find_method
+# from gen_method import gen_method
+#
+# arg = input("enter word: ")
+# gen_method(arg)
 
-arg = input("enter word: ")
-gen_method(arg)
+def split(x):
+    return [char for char in x]
+
+
+some_list = split(input('enter data: '))
+print(some_list)
--- the_3_step.py
+++ the_3_step.py
@@ -1,19 +1,27 @@
 import random
 from find_method import *
+from find_word_function import *
 
 
 def the_3_step(fact, list):
     F = []  # слово образованное в результате рандомного смешения букв
     Y = []  # список слов F
     check = 0
+    count = 0
     while check < fact:
         random.shuffle(list)  # мешаем список
         F = (''.join(list))
-        if F in Y:  # если "пар" есть в списке Y
-            continue  # начнем мешать элементы списка сначала
+        if F in Y:
+            if count > 10000:
+                break
+            # если "пар" есть в списке Y
+            else:
+                count+=1
+                continue  # начнем мешать элементы списка сначала
         else:  # если не нашел
-            Y[len(Y):] = [F]  # добавляем в конец списка  Y
-            print(F)  # печатаем F, можно распечатать и пронумеровать элементы списка Y
+            Y[len(Y):] = [F]  # добавляем в конец списка Y
+            print(f'{F} = count{count}')  # печатаем F, можно распечатать и пронумеровать элементы списка Y
+            find_in_dict(F)
             file_list = find_method()
             complete_method(file_list, F)
             # print(Y)
